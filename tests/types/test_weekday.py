e881b0a1380dd56d01ecab8004a1196a564aac9e
voidbert <humbertogilgomes@protonmail.com>
2025-07-10 19:52:58 +0100

    feat: Weekday unit tests

---
+++ tests/types/test_weekday.py
@@ -0,0 +1,7 @@
+from kepler.types.weekday import Weekday
+
+def test_repr() -> None:
+    assert repr(Weekday.WEDNESDAY) == 'Weekday.WEDNESDAY'
+
+def test_str() -> None:
+    assert str(Weekday.MONDAY) == 'Monday'
