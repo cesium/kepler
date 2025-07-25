a17e2ce7909f039646393f174fedfdb04da4827c
Voidbert <humbertogilgomes@protonmail.com>
2025-07-25 11:28:07 +0100

    feat: temporary entry point
    TODO - remove this commit later

--- kepler/__main__.py
+++ kepler/__main__.py
@@ -1,5 +1,19 @@
+import sys
+
+from .io import export_json_file, import_json_file
+from .scheduler import SchedulingProblemModel
+
 def main() -> None:
-    print('Hello, world!')
+    if len(sys.argv) != 3:
+        print('Usage: kepler in.json out.json', file=sys.stderr)
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    problem = import_json_file(input_file)
+    solution = SchedulingProblemModel(problem).solve()
+    export_json_file(output_file, solution)
 
 if __name__ == '__main__':
     main()
